c6b042d243bdad3d7e0e80c6e3a22c021ad99efc
daslu <daniel.slutsky@gmail.com>
2019-10-01 17:54:04 +0300

    changed the nlp docker image to be based on nrepl rather than jupyter

---
+++ nlp/enron-aspect-sa/src/scratch.clj
@@ -0,0 +1,116 @@
+(ns scratch
+  (:require [alembic.still :refer [distill]]
+            [clojure.walk :as walk]))
+
+(->> '[[cnuernber/libpython-clj "1.2"]
+       [panthera "0.1-alpha.16"]
+       [cheshire "5.9.0"]]
+     (map distill)
+     doall)
+
+
+(require '[cheshire.core :as cheshire]
+         '[clojure.walk :as walk]
+         '[clojure.string :as string])
+
+
+;; Ipynb to clj:
+#_
+(-> "panthera-test.ipynb"
+    slurp
+    (cheshire/parse-string true)
+    :cells
+    (->> (mapcat :source)
+         (string/join "\n")
+         (spit "draft.clj")))
+
+
+(ns panthera-test
+  (:require
+   [libpython-clj.python :as py]
+   [panthera.panthera :as pt]
+   [panthera.numpy :as np]
+   [panthera.pandas.utils :as u]
+   [clojure.pprint :refer [pprint]]
+   [clojure.repl :refer [doc]]))
+
+
+(py/initialize!)
+
+;; Wrapping Scikit Learn (sklearn)
+
+(defonce sk (py/import-module "sklearn"))
+
+(py/run-simple-string "from sklearn import *")
+
+(defn sklearn
+  ([k] ((np/module sk) k))
+  ([k args] (((np/module sk) k) args)))
+
+(def sample-data (sklearn [:datasets :make-multilabel-classification]
+                          {:random-state 0 }))
+
+
+(def X (first sample-data))
+
+(doc py/call-attr)
+
+(py/python-type X)
+
+(pprint (take 10 X))
+
+(def decomp ((sklearn [:decomposition])))
+
+(def lda (-> ((sklearn [:decomposition]))
+             (py/get-attr "LatentDirichletAllocation")))
+
+;; Wrapping Gensim
+
+(defonce gs (py/import-module "gensim"))
+
+(py/run-simple-string "from gensim.test.utils import *")
+
+(pprint (py/att-type-map gs))
+
+
+
+(defn gensim
+  ([k] ((np/module gs) k))
+  ([k args] (((np/module gs) k) args)))
+
+(def common-texts (gensim [:test :utils :common-texts]))
+
+(common-texts)
+
+(def Dictionary (-> (py/get-attr gs "corpora")
+                    (py/get-attr "dictionary")
+                    (py/get-attr "Dictionary")))
+
+(def common-dictionary (Dictionary (common-texts)))
+
+(type common-dictionary)
+
+(def common-corpus (for [t (common-texts)] (py/call-attr common-dictionary "doc2bow" t)))
+
+(map type common-corpus)
+
+(type common-corpus)
+
+common-corpus
+
+(def gensim-models (gensim [:models]))
+
+(def LdaModel (py/get-attr (gensim-models) "LdaModel"))
+
+(type common-corpus)
+
+(py/call-attr (gensim-models) "LdaModel" common-corpus)
+
+
+;; For loop of python dictionaries might be a good minimal example for Chris.
+
+(doc py/call-attr-kw)
+
+(py/call-attr-kw (gensim-models) "LdaModel" common-corpus { "num_topics" 5})
+
+(doc py/call-attr-kw)
